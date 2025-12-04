89bd894c481f33e546418de888d2d441fa828560
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2025-12-04 01:18:01 +0000

    Exemplo

--- meu-app/app/index.tsx
+++ meu-app/app/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import PaginaMestra from './screens/mestre'; // ajuste o caminho conforme onde você salvou o arquivo
+import PaginaMestra from './screens/mestre';
 
 export default function Home() {
   return <PaginaMestra />;
